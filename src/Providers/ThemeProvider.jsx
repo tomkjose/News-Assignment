60fd19e2953777ccae98e8104050e344703012e3
tomkjose <tomjosekunnumpurath@gmail.com>
2024-02-15 10:48:54 +0530

    Fix: Theme issue

--- src/Providers/ThemeProvider.jsx
+++ src/Providers/ThemeProvider.jsx
@@ -3,9 +3,12 @@ import { createContext, useContext, useState } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [currentTheme, setCurrentTheme] = useState(false);
+  const [currentTheme, setCurrentTheme] = useState(
+    localStorage.getItem("theme") || false
+  );
   const changeTheme = () => {
     setCurrentTheme(!currentTheme);
+    localStorage.setItem("theme", !currentTheme);
   };
   const theme = {
     currentTheme,
